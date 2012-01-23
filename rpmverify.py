fbbbe6766b6523d4159ee97cf4a11b86be38240d
Panos Sakkos <panos.sakkos@gmail.com>
2012-01-23 20:01:36 +0100

    Giving a shape to the source

--- rpmverify.py
+++ rpmverify.py
@@ -6,21 +6,56 @@ import rpm
 transactionSet = rpm.TransactionSet()
 headers = transactionSet.dbMatch()
 
+#foreach header in the rpm database
 for header in headers:
+
+	#for each file in the rpm header
 	files = header[rpm.RPMTAG_FILENAMES]
-	MD5sums = header[rpm.RPMTAG_FILEMD5S]
-	flags = header[rpm.RPMTAG_FILEFLAGS]
-	inodes = header[rpm.RPMTAG_FILEINODES]
-	print inodes
 
 	for i in range(len(files)):
-		stat_result = os.stat(files[i])		
-		
+        
+		#RPMTAG_FILEFLAGS related checks
+	
+		fileFlags = header[rpm.RPMTAG_FILEFLAGS]
+
+		if fileFlags[i] & rpm.RPMFILE_DOC:
+			#skip doc files
+			continue	
+                if fileFlags[i] & rpm.RPMFILE_CONFIG:
+                        #skip configuration files, they do change
+                        continue
+                if fileFlags[i] & rpm.RPMFILE_MISSINGOK:
+                        #skip doc file
+                        continue
+
+                stat_result = os.stat(files[i])
+
 		if stat.S_ISDIR(stat_result[stat.ST_MODE]):
 			#skip directories
 			continue
 		elif stat.S_ISREG(stat_result[stat.ST_MODE]):
-			print "%s with MD5 sum %s" %(files[i], MD5sums[i])
+		        #regular file	
+	
+			MD5sums = header[rpm.RPMTAG_FILEMD5S]
+        		inodes = header[rpm.RPMTAG_FILEINODES]
+        		verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
+
+			#engage
+		
+		elif stat.S_IFLNK(stat_result[stat.ST_MODE]):
+			#soft link
+			print "%s is a softlink" %(files[i])
 		else:
-			print "Skipping %s. Unknown file" %(files[i])
-	break
+			print "%s is unknown" %(files[i])
+
+
+
+
+
+
+
+
+
+
+
+
