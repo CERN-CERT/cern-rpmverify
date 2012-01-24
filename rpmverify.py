bb516b990e0ad1c75ab6f6de2d7f493ddfad1c90
Panos Sakkos <panos.sakkos@gmail.com>
2012-01-24 12:52:21 +0100

    Added rpmverify flags

--- rpmverify.py
+++ rpmverify.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 
-import os, sys, stat 
-import rpm
+import os, sys, stat, errno, rpm, rpmverify
+
+# Flags that are not defined in rpm-python.
+# They are defined in lib/rpmcli.h
+# Bit(s) for verifyFile() attributes.
+#
+
+RPMVERIFY_MD5 = 1 # 1 << 0 # /*!< from %verify(md5) */
+RPMVERIFY_FILESIZE = 2 # 1 << 1 # /*!< from %verify(size) */
+RPMVERIFY_LINKTO = 4 # 1 << 2 # /*!< from %verify(link) */
+RPMVERIFY_MTIME = 32 # 1 << 5 # /*!< from %verify(mtime) */
 
 transactionSet = rpm.TransactionSet()
 headers = transactionSet.dbMatch()
 
 #foreach header in the rpm database
 for header in headers:
-
 	#for each file in the rpm header
 	files = header[rpm.RPMTAG_FILENAMES]
 
 	for i in range(len(files)):
-        
-		#RPMTAG_FILEFLAGS related checks
-	
+
+		#RPMTAG_FILEFLAGS related checks	
 		fileFlags = header[rpm.RPMTAG_FILEFLAGS]
 
 		if fileFlags[i] & rpm.RPMFILE_DOC:
@@ -25,28 +32,51 @@ for header in headers:
                         #skip configuration files, they do change
                         continue
                 if fileFlags[i] & rpm.RPMFILE_MISSINGOK:
-                        #skip doc file
+                        #skip missing file
+                        continue
+		if fileFlags[i] & rpm.RPMFILE_README:
+                        #skip readme file
                         continue
 
-                stat_result = os.stat(files[i])
-
+                try:
+			stat_result = os.stat(files[i])
+		except:
+			#stat failed, the file does not exist
+			continue
+	
 		if stat.S_ISDIR(stat_result[stat.ST_MODE]):
 			#skip directories
 			continue
 		elif stat.S_ISREG(stat_result[stat.ST_MODE]):
 		        #regular file	
-	
-			MD5sums = header[rpm.RPMTAG_FILEMD5S]
-        		inodes = header[rpm.RPMTAG_FILEINODES]
-        		verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
-
 			#engage
-		
+	
+			MD5Sums = header[rpm.RPMTAG_FILEMD5S]
+        		inodes = header[rpm.RPMTAG_FILEINODES]
+			fileSizes = header[rpm.RPMTAG_FILESIZES]
+		        verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
+
+			if verifyFlags[i] & RPMVERIFY_MD5:
+				print "file %s failed sha256 sum check %s" %(files[i], MD5Sums[i])
+			
 		elif stat.S_IFLNK(stat_result[stat.ST_MODE]):
 			#soft link
-			print "%s is a softlink" %(files[i])
+
+		        verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
+
+			#verify that it still points to the same file
+			if verifyFlags[i] & RPMVERIFY_LINKTO:
+				print "soft link %s points to another file" %(files[i])
 		else:
 			print "%s is unknown" %(files[i])
+
+
+
+
+
+
+
+
 
 
 
