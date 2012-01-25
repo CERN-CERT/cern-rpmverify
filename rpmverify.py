81f11368dc8a7b3a18c6f46636796aa15568ddc0
Panos Sakkos <panos.sakkos@gmail.com>
2012-01-25 09:51:20 +0100

    Added SHA256 calculation

--- rpmverify.py
+++ rpmverify.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import os, sys, stat, errno, rpm
+import os, sys, stat, errno, rpm, hashlib
 
 # Flags that are not defined in rpm-python.
 # They are defined in lib/rpmcli.h
 # Bit(s) for verifyFile() attributes.
+#
 
 RPMVERIFY_MD5 = 1 # 1 << 0 #
 RPMVERIFY_FILESIZE = 2 # 1 << 1 #
@@ -16,7 +17,14 @@ S_IXGRP = 00010
 S_IXOTH = 00001
 
 def HasExecuteRights(mode):
-	return file_stat[stat.ST_MODE] & S_IXUSR or file_stat[stat.ST_MODE] & S_IXGRP or file_stat[stat.ST_MODE] & S_IXOTH
+	return file_lstat[stat.ST_MODE] & S_IXUSR or file_lstat[stat.ST_MODE] & S_IXGRP or file_lstat[stat.ST_MODE] & S_IXOTH
+
+def SHA256(filename):
+	targetFile = open(filename, "r")
+	sha256 = hashlib.sha256()
+	sha256.update(targetFile.read())
+	targetFile.close()
+	return sha256.hexdigest()
 
 transactionSet = rpm.TransactionSet()
 headers = transactionSet.dbMatch()
@@ -44,15 +52,15 @@ for header in headers:
                         continue
 
                 try:
-			file_stat = os.stat(files[i])
+			file_lstat = os.lstat(files[i])
 		except:
 			#stat failed, the file does not exist
 			continue
-	
-		if stat.S_ISDIR(file_stat[stat.ST_MODE]):
+		
+		if stat.S_ISDIR(file_lstat[stat.ST_MODE]):
 			#skip directories
 			continue
-		elif stat.S_ISREG(file_stat[stat.ST_MODE]):
+		elif stat.S_ISREG(file_lstat[stat.ST_MODE]):
 		        #regular file	
 			#engage
 		
@@ -64,18 +72,17 @@ for header in headers:
         		inodes = header[rpm.RPMTAG_FILEINODES]
 			fileSizes = header[rpm.RPMTAG_FILESIZES]
 		        verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
-
-			if not verifyFlags[i] & RPMVERIFY_MD5 and len(MD5Sums[i]) != 0:
-				print "file %s failed sha256 sum check %s" %(files[i], MD5Sums[i])
 			
-		elif stat.S_IFLNK(file_stat[stat.ST_MODE]):
-			#soft link
-
-		        verifyFlags = header[rpm.RPMTAG_FILEVERIFYFLAGS]
-	
-			#verify that it still points to the same file
-			if verifyFlags[i] & RPMVERIFY_LINKTO:
-				print "link %s points to another file" %(files[i])
+			if len(MD5Sums[i]) != 0:
+				#check sha256 sum
+				sha256 = SHA256(files[i])
+				if MD5Sums[i] != sha256:
+					print "%s SHA256 checksum missmatch inode %i size %i" %(files[i], inodes[i], fileSizes[i])
+		elif stat.S_ISLNK(file_lstat[stat.ST_MODE]):
+			#handle symbolic links
+			continue	
+		else:
+			print "%s is unknown" %(files[i])
 
 
 
